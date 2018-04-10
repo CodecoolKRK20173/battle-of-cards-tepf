9fffddb2b96c75d1315c00e0a70526a3c531a39a
edobkowski <edobkowski@gmail.com>
2018-04-10 09:41:13 +0200

    Implement Main class with neccessary imports

---
+++ src/com/codecool/Main.java
@@ -0,0 +1,29 @@
+package com.codecool;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.beans.*;
+import javafx.beans.value.*;
+import java.lang.Number;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class Main extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage stage){
+
+    }
+}
