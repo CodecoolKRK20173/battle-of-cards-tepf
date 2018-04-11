27f0b179a72d0d8e4d4a6f1648555aed6c85cd05
edobkowski <edobkowski@gmail.com>
2018-04-11 20:27:57 +0200

    Cleancode

--- src/com/codecool/Game.java
+++ src/com/codecool/Game.java
@@ -18,155 +18,75 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
 
 import com.codecool.Enums.*;
 import com.codecool.Comparators.*;
 import com.codecool.Animation.*;
 
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Game extends Pane {
-    private List<Card> deck = new ArrayList<>();
-    private List<Player> players = new ArrayList<>();
+
     private List<Pile> tableauPiles = FXCollections.observableArrayList();
     private List<Pile> playersPiles = FXCollections.observableArrayList();
-    private static double GAP = 1;
-    private Pile wastePile;
-
-    private ImageHandler imageHandler = new ImageHandler();
     private ButtonHandler buttonHandler = new ButtonHandler(this);
+    private ImageHandler imageHandler = new ImageHandler();
     private Animation animationHandler = new Animation();
+    private List<Player> players = new ArrayList<>();
+    private List<Card> deck = new ArrayList<>();
+    private Pile wastePile;
+    private double GAP = 1;
 
     public Game(List<Player> players) {
         this.players = players;
+        prepareGame();
+    }
+
+    private void prepareGame(){
+        Player firstPlayer = players.get(0);
+
         imageHandler.loadFaceCardImages();
-        this.deck = createNewDeck();
+        deck = createNewDeck();
         initPiles();
         dealCards();
-        initGame();
+        firstPlayer.activate();
+        setButtonsOnPlayer(firstPlayer);
     }
 
-    public void setTableBackground(Image tableBackground) {
-        setBackground(new Background(new BackgroundImage(tableBackground,
-                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
-                BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-    }
-
-    public boolean isGameOver() {
-        
-        if(getLostPlayersNumber() == players.size() - 1){
-            return true;
+    private List<Card> createNewDeck() {
+        HandleFile readFile = new HandleFile("test.txt");
+        List<ArrayList<Integer>> statsCollection = readFile.getStatsList();
+        List<Card> result = new ArrayList<>();
+        Image backFace = imageHandler.getBackCardImage();
+        int i = 1;
+  
+        for (ArrayList<Integer> row: statsCollection) {
+            String cardName = String.valueOf(i);
+            Image frontFace = imageHandler.getFaceCardImage(cardName);
+            result.add(new Card(cardName, backFace, frontFace, Status.FACEDOWN, row));
+            i++;
         }
+        Collections.shuffle(result);
 
-        return false;
-    }
-
-    private void setButtonsOnPlayer(Player player){
-        int xShift = 15;
-        int yShift = player.getHand().getCards().size() + 110;
-        
-        int x = (int)player.getHand().getLayoutX() + xShift;
-        int y = (int)player.getHand().getLayoutY() + yShift;
-        buttonHandler.setButtonsPosition(x, y);
-    }
-
-    private int getLostPlayersNumber(){
-        int lostPlayers = 0;
-
-        for(Player player : players){
-            if (player.getStatus().equals(Player.Status.OUT)){
-                lostPlayers++;
-            }
-        }
-
-        return lostPlayers;
-    }
-
-    public void addMouseEventHandlers(Card card) {
-        card.setOnMouseClicked(onMouseClickedHandler);
-    }
-
-    private EventHandler<MouseEvent> onMouseClickedHandler = e -> {
-        Card card = (Card) e.getSource();
-        Pile containingPile = card.getContainingPile();
-        if (containingPile.getPileType().equals(Pile.PileType.HAND)){
-            Player owner = containingPile.getOwner();
-            if (owner.isActivePlayer() && card.isFaceDown()){
-                card.flip();
-                System.out.println(card + " revealed.");
-            }
-        }
-    };
-
-    public void handleBattle(int statistic){
-        List<Card> cardsToCompare = getCardsToCompare();
-        int index = 0;
-
-        for(Card card : cardsToCompare){
-            animationHandler.slideToDest(card, tableauPiles.get(index));
-            index++;
-        }
-        
-        List<Card> sortedCards = getSortedCardsByStatistic(statistic, cardsToCompare);
-        int maxStatistic = sortedCards.get(0).getStatistic(statistic);
-        if(isDraw(sortedCards, statistic)){
-            for(Card card : sortedCards){
-                if(card.getStatistic(statistic) < maxStatistic){
-                    Player player = card.getContainingPile().getOwner();
-                    player.setStatus(Player.Status.OUT);
-                }
-                card.moveToPile(wastePile);
-            }
-        }
-        else{
-            Player winner = sortedCards.get(0).getContainingPile().getOwner();
-            for(Card card : sortedCards){
-                card.moveToPile(winner.getHand());
-            }
-            if(!wastePile.isEmpty()){
-                for(Card card : wastePile.getCards()){
-                    card.moveToPile(winner.getHand());
-                }
-            }
-            for(Player player : players){
-                if(!player.getHand().isEmpty()){
-                    player.setStatus(Player.Status.PLAYING);
-                }
-            }
-        }
-        if(isGameOver()){
-            System.out.println("Game ended");
-        }
-    }
-
-    private boolean isDraw(List<Card> cards, int statistic){
-        int firstElement = cards.get(0).getStatistic(statistic);
-        int secondElement = cards.get(1).getStatistic(statistic);
-
-        return firstElement == secondElement;
-    }
-
-    private List<Card> getCardsToCompare(){
-        List<Card> cardsToCompare = new ArrayList<>();
-        
-        for(Player player : players){
-            if (player.getStatus().isPlaying()){
-                cardsToCompare.add(player.getHand().getTopCard());
-            }
-        }
-
-        return cardsToCompare;
+        return result;
     }
 
     private void initPiles() {
+        initWastePile();
+        initTableauPiles();
+        initPlayersPiles();       
+    }
 
+    private void initWastePile(){
         wastePile = new Pile(Pile.PileType.WASTE, "Waste", GAP);
         wastePile.setBlurredBackground();
         wastePile.setLayoutX(95);
         wastePile.setLayoutY(20);
         getChildren().add(wastePile);
-        
+    }
+
+    private void initTableauPiles(){
         for (int i = 0; i < players.size(); i++) {
             Pile tableauPile = new Pile(Pile.PileType.TABLEAU, "Tableau " + i, GAP);
             tableauPile.setBlurredBackground();
@@ -175,7 +95,9 @@ public class Game extends Pane {
             tableauPiles.add(tableauPile);
             getChildren().add(tableauPile);
         }
+    }
 
+    private void initPlayersPiles(){
         int i = 0;
         double[] pilesLayoutsX = {550, players.size() == 2 ? 550 : 20, 550, 1100};
         double[] pilesLayoutsY = {510, players.size() == 2 ? 20 : 275, 20, 275};
@@ -190,13 +112,6 @@ public class Game extends Pane {
             player.setHand(playersPile);
             i++;
         }
-    }
-
-    private void initGame(){
-        Player firstPlayer = players.get(0);
-
-        firstPlayer.activate();
-        setButtonsOnPlayer(firstPlayer);
     }
 
     private void dealCards() {
@@ -216,25 +131,93 @@ public class Game extends Pane {
         }
     }
 
-    public List<Card> createNewDeck() {
-        HandleFile readFile = new HandleFile("test.txt");
-        List<ArrayList<Integer>> statsCollection = readFile.getStatsList();
-        List<Card> result = new ArrayList<>();
-        Image backFace = imageHandler.getBackCardImage();
-        int i = 1;
-  
-        for (ArrayList<Integer> row: statsCollection) {
-            String cardName = String.valueOf(i);
-            Image frontFace = imageHandler.getFaceCardImage(cardName);
-            result.add(new Card(cardName, backFace, frontFace, Status.FACEDOWN, row));
-            i++;
-        }
-        Collections.shuffle(result);
+    public void handleBattle(int statistic){
+        List<Card> cardsToCompare = getCardsToCompare();
+        List<Card> sortedCards = getSortedCardsByStatistic(statistic, cardsToCompare);
+        int maxStatistic = sortedCards.get(0).getStatistic(statistic);
 
-        return result;
+        animateCardsMovement(cardsToCompare);
+
+        if(isDraw(sortedCards, statistic)){
+            for(Card card : sortedCards){
+                if(card.getStatistic(statistic) < maxStatistic){
+                    Player player = card.getContainingPile().getOwner();
+                    player.setStatus(Player.Status.OUT);
+                }
+                card.moveToPile(wastePile);
+            }
+        }
+        else{
+            Player winner = sortedCards.get(0).getContainingPile().getOwner();
+
+            moveWinnedCards(sortedCards, winner);
+            restorePlayersToGame();
+        }
+
+        if(isGameOver()){
+            System.out.println("Game ended");
+        }
     }
 
-    public List<Card> getSortedCardsByStatistic(int stat, List<Card> cardsToCompare) {
+    private void moveWinnedCards(List<Card> cards, Player winner){
+        for(Card card : cards){
+            card.moveToPile(winner.getHand());
+        }
+        if(!wastePile.isEmpty()){
+            for(Card card : wastePile.getCards()){
+                card.moveToPile(winner.getHand());
+            }
+        }
+    }
+
+    private void restorePlayersToGame(){
+        for(Player player : players){
+            if(!player.getHand().isEmpty()){
+                player.setStatus(Player.Status.PLAYING);
+            }
+        }
+    }
+
+    private void animateCardsMovement(List<Card> cards){
+        int index = 0;
+
+        for(Card card : cards){
+            animationHandler.slideToDest(card, tableauPiles.get(index));
+            index++;
+        }
+    }
+
+    private boolean isDraw(List<Card> cards, int statistic){
+        int firstElement = cards.get(0).getStatistic(statistic);
+        int secondElement = cards.get(1).getStatistic(statistic);
+
+        return firstElement == secondElement;
+    }
+
+    private boolean isGameOver() {
+        
+        if(getLostPlayersNumber() == players.size() - 1){
+            return true;
+        }
+
+        return false;
+    }
+
+    private List<Card> getCardsToCompare(){
+        List<Card> cardsToCompare = new ArrayList<>();
+        
+        for(Player player : players){
+            if (player.getStatus().isPlaying()){
+                Card card = player.getHand().getTopCard();
+                cardsToCompare.add(card);
+                System.out.println("Card " + card + " added to cards to compare");
+            }
+        }
+
+        return cardsToCompare;
+    }
+
+    private List<Card> getSortedCardsByStatistic(int stat, List<Card> cardsToCompare) {
         switch(stat) { 
             case 0:
                 Collections.sort(cardsToCompare, new CardSortBySpd());
@@ -250,5 +233,49 @@ public class Game extends Pane {
         }
         
         return cardsToCompare;
+    }
+
+    private void setButtonsOnPlayer(Player player){
+        int xShift = 15;
+        int yShift = player.getHand().getCards().size() + 110;
+        
+        int x = (int)player.getHand().getLayoutX() + xShift;
+        int y = (int)player.getHand().getLayoutY() + yShift;
+        
+        buttonHandler.setButtonsPosition(x, y);
+    }
+
+    private int getLostPlayersNumber(){
+        int lostPlayers = 0;
+
+        for(Player player : players){
+            if (player.getStatus().equals(Player.Status.OUT)){
+                lostPlayers++;
+            }
+        }
+
+        return lostPlayers;
+    }
+
+    private void addMouseEventHandlers(Card card) {
+        card.setOnMouseClicked(onMouseClickedHandler);
+    }
+
+    private EventHandler<MouseEvent> onMouseClickedHandler = e -> {
+        Card card = (Card) e.getSource();
+        Pile containingPile = card.getContainingPile();
+        if (containingPile.getPileType().equals(Pile.PileType.HAND)){
+            Player owner = containingPile.getOwner();
+            if (owner.isActivePlayer() && card.isFaceDown()){
+                card.flip();
+                System.out.println(card + " revealed.");
+            }
+        }
+    };
+
+    public void setTableBackground(Image tableBackground) {
+        setBackground(new Background(new BackgroundImage(tableBackground,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
     }
 }
