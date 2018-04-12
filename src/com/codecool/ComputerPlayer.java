207e8eaf34883e8d16e1edf623da8e9197992b09
piotrkaminski95 <pkaminki95@gmail.com>
2018-04-12 22:03:00 +0200

    implement ComputerPlayer

--- src/com/codecool/ComputerPlayer.java
+++ src/com/codecool/ComputerPlayer.java
@@ -1,5 +1,23 @@
 package com.codecool;
 
 public class ComputerPlayer extends Player{
+    public int AiMove() {
+        return greatestStat();
+    }
 
+    private int greatestStat() {
+        Card card = this.getHand().getTopCard();
+        int firstStat = 0;
+        int numOfStat = 4;
+        int greatest = card.getStatistic(firstStat);
+        int toCompare;
+
+        for (int i = firstStat + 1; i < numOfStat; i++){
+            toCompare = card.getStatistic(i);    
+            if (toCompare > greatest) {
+                greatest = toCompare;
+            }
+        }
+        return greatest;
+    }
 }
